1ce2854a680a0e1cae73e2dad782054103da3f8c
Thomas Creagh <github@thomascreagh.mailer.me>
2025-08-06 22:21:57 +0100

    new flake file

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+
+  description = "nixos";
+  inputs = {
+    nixpkg.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+    in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
+  };
+
+}
