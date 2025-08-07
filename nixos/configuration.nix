4d932029e6c16970e67ae3da118cc8688ff08ffd
Thomas Creagh <github@thomascreagh.mailer.me>
2025-08-07 23:53:56 +0100

    2025-08-07 23:38:27  25.11.20250804.cab7782

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -100,7 +100,7 @@
     audio.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = false;
+    pulse.enable = true;
     jack.enable = true;
     wireplumber.enable = true;
   };
