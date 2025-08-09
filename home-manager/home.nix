61a47f3babdb285c94709223a0358c654df89cd8
Thomas Creagh <github@thomascreagh.mailer.me>
2025-08-09 14:06:25 +0100

    2025-08-09 11:13:52  25.11.20250804.cab7782

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -173,6 +173,10 @@
 
     settings = {
       "$mod" = "SUPER";
+      "$terminal" = "ghostty";
+      "$fileManager" = "dolphin";
+      "$webBrowser" = "brave";
+      "$menu" = "wofi --show drun";
 
       exec-once = [
         "waybar &"
@@ -181,12 +185,52 @@
       ];
 
       bind = [
-        "$mod, Q, exec, ghostty"
-        "$mod, W, exec, brave"
+        "$mod, Q, exec, $terminal"
+        "$mod, W, exec, $webBrowser"
         "$mod, K, exec, keepassxc"
         "$mod, C, killactive"
         "$mod, M, exit"
-        "$mod, F, togglefloating"
+        "$mod, E, exec, $fileManager"
+        "$mod, V, togglefloating"
+        "$mod, R, exec, $menu"
+        "$mod, P, pseudo"
+        "$mod, J, togglesplit"
+
+        # Move focus
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+
+        # Switch workspaces
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 8"
+        "$mod, 0, workspace, 10"
+
+        # Move active window to a workspace
+        "$mod, SHIFT, 1, movetoworkspace, 1"
+        "$mod, SHIFT, 2, movetoworkspace, 2"
+        "$mod, SHIFT, 3, movetoworkspace, 3"
+        "$mod, SHIFT, 4, movetoworkspace, 4"
+        "$mod, SHIFT, 5, movetoworkspace, 5"
+        "$mod, SHIFT, 6, movetoworkspace, 6"
+        "$mod, SHIFT, 7, movetoworkspace, 7"
+        "$mod, SHIFT, 8, movetoworkspace, 8"
+        "$mod, SHIFT, 9, movetoworkspace, 8"
+        "$mod, SHIFT, 0, movetoworkspace, 10"
+
+        # Special workspace
+        "$mod, S, togglespecialworkspace, magic"
+        "$mod, SHIFT S, movetoworkspace, special:magic"
+
+        # resize windows click and drag
       ];
 
       monitor = [
