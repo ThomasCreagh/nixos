232c9a8cc24dcb039ebaa89e5a11bbd7393b0778
Thomas Creagh <github@thomascreagh.mailer.me>
2025-08-19 11:24:10 +0100

    2025-08-17 21:45:25  25.11.20250804.cab7782

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -210,6 +210,7 @@
           ublock-origin
           sponsorblock
           youtube-shorts-block
+          vimium
         ];
       };
       bookmarks = {
@@ -277,6 +278,36 @@
             ];
           }
         ];
+      };
+      search = {
+        force = true;
+        default = "DuckDuckGo";
+        order = [ "DuckDuckGo" ];
+        engines = {
+          "Nix Packages" = {
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                { name = "type"; value = "packages"; }
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+            icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
+          };
+          "NixOS Wiki" = {
+            urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
+            iconUpdateURL = "https://nixos.wiki/favicon.png";
+            updateInterval = 24 * 60 * 60 * 1000; # every day
+            definedAliases = [ "@nw" ];
+          };
+          "DuckDuckGo" = {
+            urls = [{ template = "https://duckduckgo.com/?t=h_&q={searchTerms}&ia=web"; }];
+            iconUpdateURL = "https://duckduckgo.com/favicon.ico";
+            updateInterval = 24 * 60 * 60 * 1000; # every day
+            definedAliases = [ "@dk" ];
+          };
+        };
       };
     };
   };
