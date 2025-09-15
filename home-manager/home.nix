2b8c29c0cdfa5f307daf7b6d077d6d0ed9a08e2d
Thomas Creagh <github@thomascreagh.mailer.me>
2025-09-15 10:34:42 +0100

    2025-09-15 10:27:12  25.11.20250804.cab7782

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -77,6 +77,13 @@
   # programs
 
   # nvim
+  programs.fish = {
+    enable = true;
+    shellInit = ''
+      set -g fish_key_bindings fish_vi_key_bindings
+      neofetch
+    '';
+  };
   programs.neovim = {
     enable = true;
 
