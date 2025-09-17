b32a3a10fac6824456c6b30587e5c26ed792646f
Thomas Creagh <github@thomascreagh.mailer.me>
2025-09-17 20:28:23 +0100

    2025-09-17 20:23:42  25.11.20250804.cab7782

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -375,7 +375,27 @@
 
   # services
   services = {
-    mako.enable = true;
+    mako = {
+      enable = true;
+      settings = {
+        "actionable=true" = {
+          anchor = "top-left";
+        };
+        actions = true;
+        anchor = "top-right";
+        background-color = "#3a5760";
+        border-color = "#a1d1cc";
+        border-radius = 10;
+        default-timeout = 0;
+        height = 100;
+        icons = true;
+        ignore-timeout = false;
+        layer = "top";
+        margin = 10;
+        markup = true;
+        width = 300;
+      };
+    };
     swayidle = {
       enable = true;
       timeouts = [
