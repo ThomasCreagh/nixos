5f546b815d2efb369b81eedaf06e66d7702244f8
Thomas Creagh <github@thomascreagh.mailer.me>
2025-09-18 01:03:14 +0100

    2025-09-18 01:00:34  25.11.20250804.cab7782

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -430,9 +430,7 @@
         "waybar &"
         "wbg ~/.dotfiles/wallpapers/0.jpg"
         "mako"
-        "swayidle -w \
-           timeout 300 'swaylock -f -c 000000' \
-           before-sleep 'swaylock -f -c 000000'"
+        "swayidle -w timeout 300 'swaylock -f -c 000000' before-sleep 'swaylock -f -c 000000'"
       ];
 
       bind = [
