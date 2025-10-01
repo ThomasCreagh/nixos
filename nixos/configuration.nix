e8450edd7b1619d26c7664740143ee84b5b3c0cc
Thomas Creagh <github@thomascreagh.mailer.me>
2025-10-01 23:23:18 +0100

    2025-10-01 23:23:09  25.11.20250804.cab7782

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -87,6 +87,11 @@
       OVMF
       jq
       git
+      (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
+        qemu-system-x86_64 \
+          -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+          "$@"
+      '')
     ];
   };
 
