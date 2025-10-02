2d76f596b9c069e7e8c6d43ea4028485cdce6178
Thomas Creagh <github@thomascreagh.mailer.me>
2025-10-02 22:07:04 +0100

    2025-10-02 22:06:55  25.11.20250804.cab7782

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -76,31 +76,16 @@
       bash
       egl-wayland
       tree
-# virtual machine
-#      qemu
-#      qemu_kvm
-#      qemu-utils
-#      virt-viewer
-#      libguestfs
-#      jq
       git
     ];
   };
 
-  virtualisation.spiceUSBRedirection.enable = true;
   virtualisation.libvirtd = {
     enable = true;
     qemu = {
       package = pkgs.qemu_kvm;
-      runAsRoot = true;
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [(pkgs.OVMF.override {
-          secureBoot = true;
-          tpmSupport = true;
-        }).fd];
-      };
+      ovmf.enable = true;
     };
   };
 
