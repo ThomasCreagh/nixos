3158fa9784842c751ce0a4ec6736dd5982f76b02
Thomas Creagh <github@thomascreagh.mailer.me>
2025-11-30 23:10:23 +0000

    2025-11-30 23:10:14  25.11.20250804.cab7782

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -199,11 +199,11 @@
   };
   services.ofono.enable = true;
 
-  security.pam.loginLimits = [
-    { domain = "@audio"; type = "-"; item = "rtprio"; value = "99"; }
-    { domain = "@audio"; type = "-"; item = "memlock"; value = "unlimited"; }
-    { domain = "@audio"; type = "-"; item = "nice"; value = "-19"; }
-  ];
+  # security.pam.loginLimits = [
+  #   { domain = "@audio"; type = "-"; item = "rtprio"; value = "99"; }
+  #   { domain = "@audio"; type = "-"; item = "memlock"; value = "unlimited"; }
+  #   { domain = "@audio"; type = "-"; item = "nice"; value = "-19"; }
+  # ];
 
   hardware.bluetooth.enable = true;
   # steam 32 bit libs
