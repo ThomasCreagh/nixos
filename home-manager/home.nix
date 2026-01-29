8561566935eca2678eedb4f885b71e4147977ba3
Thomas Creagh <github@thomascreagh.mailer.me>
2026-01-29 14:51:13 +0000

    2026-01-29 14:16:01  26.05.20260126.bfc1b8a

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -51,8 +51,8 @@
 
     # run local ai
     (writeShellScriptBin "ai" ''
-      ollama serve&
-      DATA_DIR=~/.open-webui uvx --python 3.11 open-webui@latest serve&
+      ollama serve& >> /dev/null
+      DATA_DIR=~/.open-webui uvx --python 3.11 open-webui@latest serve& >> /dev/null
     '')
 
     # save system to github
